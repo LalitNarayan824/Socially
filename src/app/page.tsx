c84586b108725c6346ff42139f12a1a4e9259646
Lalit Narayan <mjolnirxlalit@gmail.com>
2026-01-09 12:04:04 +0530

    Day3 - added sidebar , connected auth to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <div>home page content</div>;
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function Home() {
+
+
+  const user = await currentUser();
+
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+
+      <div className="lg:grid-cols-6">
+        {user? <CreatePost/> : null}
+      </div>
+
+      <div className="hidden lg:block lg:grid-cols-4">
+        {/* <WhoToFollow/> */}
+        who to follow
+
+      </div>
+
+    </div>
+  );
 }
