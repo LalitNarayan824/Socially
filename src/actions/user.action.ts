8db9b1ee6b4a80e98bd49d163977ba990014844a
Lalit Narayan <mjolnirxlalit@gmail.com>
2026-01-11 19:42:21 +0530

    Trial Project Completed

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -64,7 +64,7 @@ export async function getUserByClerkId(clerkId : string){
 export async function getDbUserId(){
 
   const {userId : clerkId} = await auth();
-  if(!clerkId) throw new Error("Unauthorized");
+  if(!clerkId) return null;
 
   const user = await getUserByClerkId(clerkId);
 
@@ -76,7 +76,9 @@ export async function getDbUserId(){
 
 export async function getRandomUsers(){
   try {
-    const userId = await getDbUserId()
+    const userId = await getDbUserId();
+
+    if(!userId) return [];
 
     // get 3 random users excluding ourselves and users we already follow
 
